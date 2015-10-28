4305cfff27e52cc9babb4d7dc4ccae391a4cf670
artests <artests87@gmail.com>
2015-10-28 14:41:05 +0300

    The end!

--- app/src/main/java/com/example/artests/l22camera/MainActivity.java
+++ app/src/main/java/com/example/artests/l22camera/MainActivity.java
@@ -1,52 +1,102 @@
 package com.example.artests.l22camera;
 
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    final int CAMERA_CAPTURE = 1;
+    final int PIC_CROP = 2;
+    private Uri picUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setTitle("Съёмка и Кадрирование");
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onClick(View v) {
+        try {
+            // Намерение для запуска камеры
+            Intent captureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(captureIntent, CAMERA_CAPTURE);
+        } catch (ActivityNotFoundException e) {
+            // Выводим сообщение об ошибке
+            String errorMessage = "Ваше устройство не поддерживает съемку";
+            Toast toast = Toast
+                    .makeText(this, errorMessage, Toast.LENGTH_SHORT);
+            toast.show();
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            // Вернулись от приложения Камера
+            if (requestCode == CAMERA_CAPTURE) {
+                // Получим Uri снимка
+                picUri = data.getData();
+                // кадрируем его
+                performCrop();
+            }
+            // Вернулись из операции кадрирования
+            else if(requestCode == PIC_CROP){
+                Bundle extras = data.getExtras();
+                // Получим кадрированное изображение
+                Bitmap thePic = extras.getParcelable("data");
+                // передаём его в ImageView
+                ImageView picView = (ImageView)findViewById(R.id.picture);
+                picView.setImageBitmap(thePic);
+            }
+        }
+    }
+
+    private void performCrop(){
+        try {
+            // Намерение для кадрирования. Не все устройства поддерживают его
+            Intent cropIntent = new Intent("com.android.camera.action.CROP");
+            cropIntent.setDataAndType(picUri, "image/*");
+            cropIntent.putExtra("crop", "true");
+            cropIntent.putExtra("aspectX", 1);
+            cropIntent.putExtra("aspectY", 1);
+            cropIntent.putExtra("outputX", 300);
+            cropIntent.putExtra("outputY", 350);
+            cropIntent.putExtra("return-data", true);
+            startActivityForResult(cropIntent, PIC_CROP);
+        }
+        catch(ActivityNotFoundException anfe){
+            String errorMessage = "Извините, но ваше устройство не поддерживает кадрирование";
+            Toast toast = Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 }
